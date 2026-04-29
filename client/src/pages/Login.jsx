a114997975fbc03b57b7ce5ff602c3f9c7a82197
Mark Mutwiri <Markelmutwiri144@gmail.com>
2026-04-29 19:48:00 +0300

    Fetch

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ username: '', password: '' });
